5345ea76ac013030367d842974794e9f6f1251bd
Jeevan MB <jforcode@gmail.com>
2022-04-03 01:05:26 +0530

    simple click based tool to show which tiles are yet to come

---
+++ Assets/Blocker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker : MonoBehaviour
+{
+
+    bool blockerOn = false;
+
+    private void OnMouseUp()
+    {
+        blockerOn = !blockerOn;
+        var c = GetComponent<SpriteRenderer>().color;
+        c.a = blockerOn ? 1 : 0;
+        GetComponent<SpriteRenderer>().color = c;
+    }
+}
